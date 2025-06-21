a4ba65c297dbac3aad9ecfba86ba5c4d0f6dcd7b
saadsaifullah <saad.saifullah.m@gmail.com>
2025-06-21 02:10:18 -0700

    Update project for deployment

--- App.tsx
+++ App.tsx
@@ -1,44 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from "./src/components/Header";
-import Home from "./src/components/Hero";
-import Steps from "./src/components/Steps";
-import Features from "./src/components/Features";
+import Hero from "./src/components/Hero";
 import CTA from "./src/components/CTA";
 import Footer from "./src/components/Footer";
-import HowItWorks from './src/components/HowItWorks'; // newly added
+import HowItWorks from './src/components/HowItWorks';
 import Subscribe from './src/components/Subscribe';
-import CaptionTool from './src/components/CaptionTool'; // 👈 rename the second App.tsx to this
+import CaptionTool from './src/components/CaptionTool';
 import Signup from './src/components/Signup';
 import Login from './src/components/Login';
-import ProtectedRoute from './src/components/ProtectedRoute';
 import Success from './src/Success';
-import { AuthProvider } from './src/context/AuthContext';
 import Profile from './src/components/Profile';
-
+import ProtectedRoute from './src/components/ProtectedRoute';
+import { AuthProvider } from './src/context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-[#0d1117] text-white">
           <Header />
           <main className="flex-grow pt-24">
             <Routes>
-              {/* Public Routes */}
+              {/* Home Page */}
               <Route path="/" element={
                 <>
-                  <Home />
+                  <Hero />
                   <CTA />
                 </>
               } />
+
+              {/* Public Pages */}
               <Route path="/how-it-works" element={<HowItWorks />} />
               <Route path="/subscribe" element={<Subscribe />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />} />
               <Route path="/success" element={<Success />} />
-<Route path="/profile" element={<Profile />} />
-              {/* Protected Route */}
+              <Route path="/profile" element={<Profile />} />
+
+              {/* Protected Route for Caption Tool */}
               <Route
                 path="/caption-tool"
                 element={
@@ -57,4 +57,3 @@ function App() {
 }
 
 export default App;
-
